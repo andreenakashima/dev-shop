885a3c0b1616080615ab4f2c160d1c7ec3189dc3
andreenakashima <andreenakashima@gmail.com>
2024-01-26 16:13:17 -0300

    Header component

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -3,7 +3,7 @@ import { Header } from "../components/header";
 
 const StoreLayout = ({ children }: { children: ReactNode }) => {
 	return (
-		<div>
+		<div className="mx-auto min-h-screen grid w-full max-w-[1600px] grid-rows-app gap-5 p-8">
 			<Header />
 			{children}
 		</div>
