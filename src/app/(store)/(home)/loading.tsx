fc5af3ad7d9c26a7c4085beea41d2e0db846abe5
andreenakashima <andreenakashima@gmail.com>
2024-01-29 16:50:24 -0300

    Loading in Home page

--- src/app/(store)/(home)/loading.tsx
+++ src/app/(store)/(home)/loading.tsx
@@ -1,5 +1,11 @@
-const Loading = () => {
-	return <div>Loading</div>;
-};
+import { Skeleton } from "@/components/skeleton";
 
-export default Loading;
+export default function HomeLoading() {
+	return (
+		<div className="grid h-full grid-cols-9 grid-rows-6 gap-6">
+			<Skeleton className="col-span-6 row-span-6 h-[856px]" />
+			<Skeleton className="col-span-3 row-span-3" />
+			<Skeleton className="col-span-3 row-span-3" />
+		</div>
+	);
+}
