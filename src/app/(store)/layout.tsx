8d14430db3b83418836319b9fbae373668f23389
andreenakashima <andreenakashima@gmail.com>
2024-01-29 15:02:05 -0300

    Create fetch api wrapper

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from "react";
-import { Header } from "../components/header";
+import { Header } from "../../components/header";
 
 const StoreLayout = ({ children }: { children: ReactNode }) => {
 	return (
