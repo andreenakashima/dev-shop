1e788156b4a87da43290e603c365d3c81dbe2b28
andreenakashima <andreenakashima@gmail.com>
2024-01-31 09:45:01 -0300

    Cart context

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,12 +1,15 @@
 import React, { ReactNode } from "react";
 import { Header } from "../../components/header";
+import { CartProvider } from "@/context/cart-context";
 
 const StoreLayout = ({ children }: { children: ReactNode }) => {
 	return (
-		<div className="mx-auto min-h-screen grid w-full max-w-[1600px] grid-rows-app gap-5 p-8">
-			<Header />
-			{children}
-		</div>
+		<CartProvider>
+			<div className="mx-auto min-h-screen grid w-full max-w-[1600px] grid-rows-app gap-5 p-8">
+				<Header />
+				{children}
+			</div>
+		</CartProvider>
 	);
 };
 
